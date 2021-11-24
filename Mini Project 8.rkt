17340814a406d37f69682a77bc5f91d52984cfc6
Elliot Swaim <swaimell@grinnell.edu>
2021-11-24 07:58:58 -0600

    Restoring old version

--- Mini Project 8.rkt
+++ Mini Project 8.rkt
@@ -1,4 +1,6 @@
 #lang racket
+(require csc151)
+(require csc151/rex)
 
 #| STRUCTS FOR PARAGRAPHS, SENTENCES, WORDS, AND conjuclentionS |#
 
@@ -84,18 +86,36 @@
 ;;;  make it give a default value, such as clasifying all words as singular,
 ;;;  neuter, nominative nouns.
 ;;; TODO: Make this actually word (during part two)
-(define string->conjuclention
-  (lambda (str)
-    (declention 'singular 'neuter 'nominative))) 
 
 ;;; (string->word str) -> word?
 ;;;    str : string?
 ;;; Converts a string to a word struct.
 ;;;  Assumes str contains one word
-(define string->word
-  (lambda (str)
-    (word str
-          (string->conjuclention str))))
+
+
+;;; (string->words-list str) -> list? of string?
+;;;   str : string?
+;;; Creates a list of words represented by strings
+(define string->words-list
+  (let* ([rex-alphabetical (rex-any-of (rex-char-range #\a #\z)
+                                       (rex-char-range #\A #\Z))]
+         [rex-contractions (rex-concat (rex-repeat rex-alphabetical)
+                                       (rex-char-set "'")
+                                       (rex-repeat rex-alphabetical))])
+    (lambda (str)
+      (rex-find-matches (rex-any-of rex-contractions
+                                    (rex-repeat rex-alphabetical))
+                        str))))
+
+(test-equal? "Empty string"
+             (string->words "")
+             '(""))
+(test-equal? "Hyphens, Contractions, and Punctuation"
+             (string->words "Hello")
+             '("Hello"))
+(test-equal? "Hyphens, Contractions, and Punctuation"
+             (string->words "Test test-ing nope- -not don't 'try this' Punctuation? Not.Fun")
+             '("Test" "test" "ing" "nope" "not" "don't" "try" "this" "Punctuation" "Not" "Fun"))
 
 ;;; (string->sentence str) -> sentence?
 ;;;   str : string?
